758ddc40183763b078a2743fb6e876c3e70fa846
the-other-mariana <mariana.avalos.arce@gmail.com>
2024-01-25 23:42:03 +0000

    update: algo 1 done
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace std;
+using namespace cv;
+int isqrt(int x) {
+    int r = x;
+    while (r * r > x) {
+        r = (r + x / r) / 2;
+    }
+    return r;
+}
+
+void algo1(Mat* img, int radius, int cx, int cy) {
+for(int y=-radius; y<=radius; y++)
+    for(int x=-radius; x<=radius; x++)
+        if(x*x+y*y <= radius*radius)
+            img->at<uchar>(cx+x, cy+y) = 255;
+}
 
 int main() {
-	cout << "hello world" << endl;
+	Mat matrix(500, 500, CV_8U, cv::Scalar(0));
+	algo1(&matrix, 5, 100, 100);
+	imwrite("algo1.png", matrix);
 }
