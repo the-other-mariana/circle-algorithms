e99ea12948cafb003f6f45c53f74db30180dc995
the-other-mariana <mariana.avalos.arce@gmail.com>
2024-01-25 20:11:14 -0600

    update: plot stuff
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ src/plot/plot.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Replace 'your_file.csv' with the path to your CSV file
+file_path = '../measures.csv'
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path)
+print(df.keys())
+
+# Plotting
+plt.figure(figsize=(10, 6))
+
+# Iterate over unique 'calls' values and plot a line for each
+for calls in df['calls'].unique():
+    subset = df[df['calls'] == calls]
+    plt.plot(subset['algorithm_id'], subset['time(ms)'], label=f'{calls} calls')
+
+plt.title('Algorithm Execution Time')
+plt.xlabel('Algorithm ID')
+plt.ylabel('Time (ms)')
+plt.legend()
+plt.grid(True)
+plt.xticks(df['algorithm_id'].unique())
+plt.savefig("../plot.png")
+plt.show()
